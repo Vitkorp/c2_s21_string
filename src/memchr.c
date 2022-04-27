4aea1ffbad44544a1943e1bc4a357009ddf6cca4
HenRok1 <naderbaev.nail@mail.ru>
2022-04-27 15:05:25 +0300

    Реализация первой функции

--- src/memchr.c
+++ src/memchr.c
@@ -3,9 +3,14 @@
 void s21_memchr(const void *str, int c, size_t n);
 
 int main() {
-
+    
 }
 
 void s21_memchr(const void *str, int c, size_t n) {
-    
+    size_t i;
+    for (i = 0; i < n; i++) {
+        if (*((char *)str + i) == c)
+            return (char *) str + i;
+    }
+    return NULL;
 }
