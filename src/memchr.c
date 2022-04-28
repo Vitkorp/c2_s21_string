5e0202fd0584dcaac44a367d6ddc5931fab8d63a
roberter <roberter@student.21-school.ru>
2022-04-28 19:15:35 +0300

    test

--- src/memchr.c
+++ src/memchr.c
@@ -2,7 +2,7 @@
 
 void s21_memchr(const void *str, int c, size_t n);
 int memcmp(const void *str1, const void *str2, size_t n);
-
+void *memcpy(void *dest, const void *src, size_t n);
 
 int main() {
 
@@ -16,4 +16,14 @@ void s21_memchr(const void *str, int c, size_t n) {
     return NULL;
 }
 
-int 
+int memcmp(const void *str1, const void *str2, size_t n) {
+    char *dest = str1;
+    const char *src = str2;
+    while (n--)
+        *dest++ = *src++;
+    return dest;
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+
+}
