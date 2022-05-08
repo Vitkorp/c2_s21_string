17247b9d677cbda1e5664a74140c04c35eb8237a
ondrewvi <ondrewvi@student.21-school.ru>
2022-05-08 06:31:42 -0700

    .c and upper lower

--- src/s21_string.c
+++ src/s21_string.c
@@ -2,18 +2,20 @@
 #include <string.h>
 #include "s21_string.h"
 
+typedef long unsigned s21_size_t;
+
 // копирует n сивловов из src в dest
-void *s21_memcpy(void *dest, const void *src, size_t n) {
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
     char *to = dest;
     const char *from = src;
-    for (size_t i = 0; i < n; i++) {
+    for (s21_size_t i = 0; i < n; i++) {
         *to++ = *from++;
     }
     return dest;
 }
 
 // копирует n сивловов из src в dest. whithout overlap
-void *s21_memmove(void *dest, const void *src, size_t n) {
+void *s21_memmove(void *dest, const void *src, s21_size_t n) {
     char *to = dest;
     const char *from = src;
 
@@ -21,7 +23,7 @@ void *s21_memmove(void *dest, const void *src, size_t n) {
     const char *from_end = (from + n - 1);
 
     if (to < from) {
-        for (size_t i = 0; i < n; i++) {
+        for (s21_size_t i = 0; i < n; i++) {
         *to++ = *from++;
         }
     } else {
@@ -34,9 +36,9 @@ void *s21_memmove(void *dest, const void *src, size_t n) {
 }
 
 // Копирует символ c (беззнаковый тип) в первые n символов строки, на которую указывает аргумент str.
-void *s21_memset(void *src, int c, size_t n) {
+void *s21_memset(void *src, int c, s21_size_t n) {
     char *to = src;
-    for (size_t i = 0; i < n; i++) {
+    for (s21_size_t i = 0; i < n; i++) {
         *to++ = c;
     }
     return src;
@@ -54,8 +56,8 @@ char *s21_strcpy(char *dest, const char *src) {
 }
 
 // Копирует до n символов из строки, на которую указывает src, в dest.
-char *s21_strncpy(char *dest, const char *src, size_t n) {
-    size_t i;
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
+    s21_size_t i;
     for (i = 0; i < n; i++) {
         if (src[i] != '\0') {
             dest[i] = src[i];
@@ -77,7 +79,7 @@ void *s21_to_upper(const char *str) {
         flag = 0;
     } else {
         s21_strcpy(dest, str);
-        for (size_t j = 0; j < strlen(str); j++) {
+        for (s21_size_t j = 0; j < strlen(str); j++) {
             if (('a' <= dest[j] && 'z' >= dest[j])) {
                 dest[j] += 'A' - 'a';
             }
@@ -104,7 +106,7 @@ void *s21_to_lower(const char *str) {
         flag = 0;
     } else {
         s21_strcpy(dest, str);
-        for (size_t j = 0; j < strlen(str); j++) {
+        for (s21_size_t j = 0; j < strlen(str); j++) {
             if (('A' <= dest[j] && 'Z' >= dest[j])) {
                 dest[j] += 'a' - 'A';
             }
