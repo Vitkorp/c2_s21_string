bdaa37b9603c23b31cf0811fc92a892951126a7f
ondrewvi <ondrewvi@student.21-school.ru>
2022-05-08 06:30:59 -0700

    .h

--- src/s21_string.h
+++ src/s21_string.h
@@ -3,11 +3,13 @@
 #define SRC_S21_STRING_H_
 #include <stdio.h>
 
-void *s21_memcpy(void *dest, const void *src, size_t n);
-void *s21_memmove(void *dest, const void *src, size_t n);
-void *s21_memset(void *str, int c, size_t n);
+typedef long unsigned s21_size_t;
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n);
+void *s21_memmove(void *dest, const void *src, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
 char *s21_strcpy(char *dest, const char *src);
-char *s21_strncpy(char *dest, const char *src, size_t n);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 void *s21_to_upper(const char *str);
 void *s21_to_lower(const char *str);
 
