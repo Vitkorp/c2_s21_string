985335cf5417c94f8de7828e0d491568d5b23efb
Ondrew Violator <ondrewvi@vo-k4.msk.21-school.ru>
2022-05-21 17:05:47 +0300

    прототип фукнции s21_sprintf. Описание структуры формата, для обработки конвертерами

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,12 +1,69 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "s21_sprintf.h"
+#include "s21_string.h"
+#include <stdio.h>
+
+int endfmt(const char *str, int start) {
+    int _pos = start;
+
+    return _pos;
+}
+
+// int sprintf(char *str, const char *format, ...) 
+int s21_sprintf(char *str, const char *format, ...) {
+    int count = 0;
+    fmt formats;
+    int i_src = 0;
+    s21_size_t slen = s21_strlen(str);
+    char *tmp = S21_NULL;
+    while (format[i_src]) {
+        printf("i_src={%d}, char={'%c'}     ", i_src, format[i_src]);
+        if (format[i_src] != '%') {
+            count++;
+            // if (slen <= (s21_size_t)count) {
+            //     tmp = realloc(str, count * sizeof(char));
+            //     str = tmp;
+            // }
+            // str[count - 1] = format[i_src];
+            // printf("str[%d]='%c',    format[%d]='%c'\n", count, str[count], i_src, format[i_src]);
+            
+            printf("\n");
+        } else {
+            int startFmtPosition = i_src + 1;
+            int endFmrPosition = endfmt(format, startFmtPosition);
+            printf("startFmtPosition={%d}, endFmrPosition={%d}\n", startFmtPosition, endFmrPosition);
+        }
+        i_src++;
+    }   
+
+    return count;
+}
 
 int main() {
     int a = 5;
-    char s[50] = "";
-    sprintf(s, "Hello %-------+#######10.5d endl",  a);
+    char *s;
+    s21_sprintf(s, "Heool!!!!!!! %+-10.5 d ddddddn\n", a);
     printf("%s", s);
+    printf("\n\n\n\n\n");
+    sprintf(s, "Hello %----+#######8.5 lj k  %endl\n");
+    printf("%s", s);
+    // sprintf(s, "Hello %\n");
+    // printf("%s", s);
+    // sprintf(s, "%e\n");
+    // printf("%s", s);
+    // sprintf(s, "%s\n");
+    // printf("%s", s);
+    // sprintf(s, "%c\n");
+    // printf("%s", s);
+    // sprintf(s, "%d\n");
+    // printf("%s", s);
+    // sprintf(s, "%lu\n");
+    // printf("%s", s);
+    // sprintf(s, "%llu\n");
+    // printf("%s", s);
+    // sprintf(s, "%lld\n");
+    // printf("%s", s);
     return 0;
 }
 
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -1,11 +1,48 @@
 #include <string.h>
 
 // %[флаги][ширина][.точность][длина]спецификатор
-typedef struct _flags {
-    char flag_minus;
-    char flag_plus;
-    char space;
-    char hash;
-    char 
 
-} flags;
+// {-} {+} {' '} {#} {0}
+// количество вхождений флага
+typedef struct _flags {
+    int minus;
+    int plus;
+    int space;
+    int hash;
+    int zero;
+} fl;
+
+// {0..9} {*} 
+// количество вхождений флага ширины
+typedef struct _width {
+    int number;
+    int starchar;
+} w;
+
+// {0..9} {*} 
+// количество вхождений флага точности
+typedef struct _precision {
+    int number;
+    int starchar;
+} pr;
+
+// {h} {l} {L}
+// количество вхождений флага длины
+typedef struct _length {
+    int l_short;
+    int starchar;
+} len;
+
+typedef struct _format{
+    fl flags;
+    w  width;
+    pr precision;
+    len length;
+    char spec;
+} fmt;
+
+// int sprintf(char *str, const char *format, ...) 
+int s21_sprintf(char *str, const char *format, ...);
+
+//  позиция указателя до конца подстроки замещаемой части формата
+int endfmt(const char *str, int start);
