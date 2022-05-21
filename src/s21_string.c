2b8b04e7d5c17920bb3afe3d10f156ba34bf1695
Robert Erlene <roberter@vo-k5.msk.21-school.ru>
2022-05-21 16:37:36 +0300

    tests

--- src/s21_string.c
+++ src/s21_string.c
@@ -56,66 +56,6 @@ char *s21_strrchr(const char *str, int c) {
     return found ? ((char *)str + i) : S21_NULL;
 }
 
-// char *s21_strstr(const char *haystack, const char *needle) {
-//     int exit_code = 0;
-//     char *_res = "";
-    
-//     // if (!*needle){
-//     //     exit_code = -1;
-//     const char needle_first  = *needle;
-
-//     while (True) {
-
-//         haystack = s21_strchr(haystack, needle_first);
-//         if (!haystack){
-//             exit_code = 1; // S21_NULL
-//         }
-//         const char  *i_haystack = haystack + 1,
-//                     *i_needle = needle + 1;
-
-//         unsigned int sums_diff = *haystack;
-//         bool identical = true;
-
-//         while (*i_haystack && *i_needle) {
-//             sums_diff += *i_haystack;
-//             sums_diff -= *i_needle;
-//             identical &= *i_haystack++ == *i_needle++;
-//         }
-
-//         if (*i_needle){
-//             exit_code = 1; // S21_NULL
-//         }
-//         else if (identical){
-//             exit_code = 2; //(char *)haystack
-//         }
-//         const char *sub_start;
-
-//         size_t needle_len = i_needle - needle;
-//         size_t needle_len_1 = needle_len - 1;
-
-//         for (sub_start = haystack; *i_haystack; i_haystack++) {
-//             sums_diff -= *sub_start++;
-//             sums_diff += *i_haystack;
-
-//             if (sums_diff == 0 && needle_first == *sub_start && memcmp(sub_start, needle, needle_len_1) == 0) { //поменять на s21_memcmp
-//                 exit_code = 3; // (char*) sub_start
-//             }
-//         }
-//     }
-//     if (exit_code == 2){
-//         _res = (char *)haystack;
-//         exit_code = 1;
-//     }
-//     else if (exit_code == 3){
-//         _res = (char *)sub_start;
-//         exit_code = 1;
-//     }
-//     // else if (exit_code == 1)
-//     //     _res = S21_NULL;
-
-//     return (exit_code) ? _res : S21_NULL;
-// }
-
 char *s21_strstr(const char *haystack, const char *needle) {
     const size_t needle_len = s21_strlen(needle);
     char *res = "";
