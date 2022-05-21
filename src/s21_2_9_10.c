5dd6a5a1bd88afdea0fdc3293475e93102eece7c
Howie Teressa <howieter@vo-e3.msk.21-school.ru>
2022-05-22 02:50:04 +0300

    s21mem s21strn - fixed

--- src/s21_2_9_10.c
+++ src/s21_2_9_10.c
@@ -1,15 +1,15 @@
 #include "s21_2_9_10.h"
 
-// int main() {
-//     const char* str1 = "1234567890";
-//     const char* str2 = "12341567890";
-//     if (s21_strcmp(str1, str2) == 0) {
-//         puts("equal");
-//     } else {
-//         puts("different");
-//     }
-//     printf("%d", s21_strcmp(str1, str2));
-// }
+int main() {
+    const char* str1 = "lo world";
+    const char* str2 = "lo";
+    if (s21_strcmp(str1, str2) == 0) {
+        puts("equal");
+    } else {
+        puts("different");
+    }
+    printf("%d", s21_memcmp(str1, str2, 2));
+}
 
 int s21_memcmp(const void *str1, const void *str2, size_t n) {
     const char* string1 = (char*)str1;
@@ -17,6 +17,9 @@ int s21_memcmp(const void *str1, const void *str2, size_t n) {
     int flag = 0;
     size_t i = 0;
     while (i < n) {
+        if (i == n - 1 && string1[i] == string2[i]) {
+            break;
+        }
         if (string1[i] == string2[i]) {
             i++;
         } else {
@@ -32,27 +35,12 @@ int s21_memcmp(const void *str1, const void *str2, size_t n) {
 }
 
 int s21_strncmp(const char *str1, const char *str2, size_t n) {
-    // size_t i = 0;
-    // while (i < n) {
-    //     if (str1[i] == str2[i]) {
-    //         i++;
-    //     } if (str1[i] == '\0' || str2[i] == '\0' || str1[i] != str2[i]) {
-    //         break;
-    //     }
-    // }
-    // int size_str1 = strlen(str1);
-    // int size_str2 = strlen(str2);
-    // if (str1[i] == str2[i] || size_str1 == size_str2) {
-    //     i = 0;
-    // } else if (size_str1 > size_str2) {
-    //     i = (int)str1[i] - (int)str2[i];
-    // } else if (size_str1 < size_str2) {
-    //     i = ((int)str2[i] - (int)str1[i])* (-1);
-    // }
-    // return i;
     int flag = 0;
     size_t i = 0;
     while (i < n) {
+        if (i == n - 1 && str1[i] == str2[i]) {
+            break;
+        }
         if (str1[i] == str2[i]) {
             i++;
         } else {
